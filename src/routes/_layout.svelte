44116075b6f641cdb3fc8a4e24a23cf01fe9b0fd
rgfx <raphael@rgfx.net>
2019-08-29 10:26:34 -0600

    init commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+</script>
+
+<style lang="scss" global>
+  @import "./style/mixins.scss";
+  @import "./style/main.scss";
+  main {
+    position: relative;
+    max-width: 56em;
+    //background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+    @include bp(medium) {
+      // background-color: #444;
+    }
+  }
+</style>
+
+<Nav {segment} />
+
+<main>
+  <slot />
+</main>
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+const sveltePreprocess = require("svelte-preprocess");
+
+module.exports = {
+  preprocess: sveltePreprocess({
+    scss: {
+      includePaths: ["node_modules", "src"]
+    },
+    postcss: {
+      plugins: [require("autoprefixer")]
+    }
+  })
+  // ...other svelte options
+};
